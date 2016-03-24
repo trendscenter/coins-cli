1ead708647b59bf647157edb0325b4fd07e48550
cdieringer3 <cdieringer3@gatech.edu>
2016-03-24 13:32:04 -0600

    remove bogus console output

--- src/format-output.js
+++ src/format-output.js
@@ -3,7 +3,6 @@ const firstLine = (str) => {
 }
 
 module.exports = (verbose, stderr, stdout) => {
-  console.log('is verbose?', verbose)
   const output = stdout + stderr
   if (verbose) return output
   return firstLine(stdout) + firstLine(stderr)
